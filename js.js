e0f1a0b2fbf4d36e26fa133ec21c975c165a5589
jyothikiran843 <jyothikiranthammana843@gmail.com>
2022-07-14 22:53:55 +0530

    div added

---
+++ js.js
@@ -0,0 +1,8 @@
+Array.from(document.getElementsByName('input')).forEach(item=>{
+    item.addEventListener('change',inputChecked)
+})
+function inputChecked(){
+    if(this.value=='Yes'){
+        
+    }
+}
