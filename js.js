04e05ffc95b79522248f4f727828a087ed2ee9df
jyothikiran843 <jyothikiranthammana843@gmail.com>
2022-07-15 22:29:28 +0530

    footer and js

--- js.js
+++ js.js
@@ -1,7 +1,35 @@
+var allradios=Array.from(document.getElementsByClassName('radio-input'));
+allradios.forEach(ele=>{
+    ele.addEventListener('click',radioChecked);
+})
+Array.from(document.getElementsByClassName('sodi-text')).forEach(inp=>{
+    inp.addEventListener('keyup',errorProcess)
+})
+function errorProcess(){
+    if(this.value){
+        this.nextElementSibling.nextElementSibling.style.visibility="hidden"
+    }
+    else{
+        this.nextElementSibling.nextElementSibling.style.visibility="visible"
+    }
+}
 function yes(value){
     document.getElementById("second-div"+value).style.display="block"
-}
 
+}
 function no(value){
     document.getElementById("second-div"+value).style.display="none"
+}
+function radioChecked(){
+    var progrss=39;
+    allradios.forEach(rad=>{
+        if(rad.checked){
+            progrss++;
+            document.getElementById('progressed').style.width=progrss+'%'
+            document.getElementById('answeredare').innerHTML=progrss
+        }
+        else{
+            console.log(rad.checked)
+        }
+    })
 }
