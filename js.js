c992896786e5eb34e93d1d13c3c4dfa7c4b1c8c5
jyothikiran843 <jyothikiranthammana843@gmail.com>
2022-07-15 16:45:58 +0530

    js

--- js.js
+++ js.js
@@ -1,8 +1,7 @@
-Array.from(document.getElementsByName('input')).forEach(item=>{
-    item.addEventListener('change',inputChecked)
-})
-function inputChecked(){
-    if(this.value=='Yes'){
-        
-    }
+function yes(value){
+    document.getElementById("second-div"+value).style.display="block"
+}
+
+function no(value){
+    document.getElementById("second-div"+value).style.display="none"
 }
